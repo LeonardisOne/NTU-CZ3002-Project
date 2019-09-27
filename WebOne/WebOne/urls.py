641eaf62ab7ef78e8f8d9376ff962c098f7e17b6
DanWijaya <danwijaya19@gmail.com>
2019-09-28 02:17:08 +0800

    changed

--- WebOne/WebOne/urls.py
+++ WebOne/WebOne/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from appOne import views
-from django.conf.urls import include
+from django.conf.urls import include, url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- WebOne/appOne/models.py
+++ WebOne/appOne/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    user_name = models.CharField(max_length = 128)
-    password = models.CharField(max_length = 32)
-    last_name = models.CharField(max_length = 128)
+# class User(models.Model):
+#     user_name = models.CharField(max_length = 128)
+#     password = models.CharField(max_length = 32)
+#     last_name = models.CharField(max_length = 128)
