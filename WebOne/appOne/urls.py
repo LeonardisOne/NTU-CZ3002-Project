79642c1a4c844a2bd57ab0c4fe74a21026d11f56
LeonardIsOne <32675659+LeonardisOne@users.noreply.github.com>
2019-10-16 13:22:37 +0800

    add missing stuff for view chapter results

--- WebOne/appOne/urls.py
+++ WebOne/appOne/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('modules/delete_module/<module_pk>/',views.delete_module, name='delete_module'),
     path('modules/<str:pk>/chapters/<str:pq>/publish/', views.publish_chapter, name='publish_chapter'),
     path('<m_name>/<ch_name>/<int:q_num>/',views.start_question, name='start_question'),
+    path('<m_name>/<ch_name>/view_result/', views.view_chapter_result, name='view_result'),
 ]
 """
 urlpatterns = [
